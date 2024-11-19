6f06804d665151018ba03094aa91de012130368c
tgt <tgt@incognitotgt.me>
2024-11-19 18:44:49 -0500

    first commit

---
+++ schoology/schoologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  schoologyApp.swift
+//  schoology
+//
+//  Created by Mashoor Ahmed on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct schoologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
